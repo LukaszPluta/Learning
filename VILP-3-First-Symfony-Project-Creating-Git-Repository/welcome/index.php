672eab9ad437a8a2d1aaf4e7834cb9e0449b530a
lpluta <l.pluta@wearevirtua.com>
2019-01-18 09:24:27 +0100

    change starting page

---
+++ VILP-3-First-Symfony-Project-Creating-Git-Repository/welcome/index.php
@@ -0,0 +1,6 @@
+<?php
+echo '<h1>HURRAAA UDALO SIE!!</h1>';
+
+echo 'Aktualnie mamy date: '.date('m/d/Y h:i:s a', time());
+
+echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam corporis culpa cumque debitis dolorem dolores doloribus excepturi expedita hic obcaecati, perferendis quae quidem quo quod ratione repudiandae sit temporibus totam?</p>';
