0202dff4d4afc423d43b60636e2b1152606933ac
lpluta <l.pluta@wearevirtua.com>
2019-01-18 13:07:39 +0100

    VILP-9

---
+++ VILP-3-First-Symfony-Project-Creating-Git-Repository/src/Entity/Telefon.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TelefonRepository")
+ */
+class Telefon
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripton;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateOfCreation;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateLastMod;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescripton(): ?string
+    {
+        return $this->descripton;
+    }
+
+    public function setDescripton(string $descripton): self
+    {
+        $this->descripton = $descripton;
+
+        return $this;
+    }
+
+    public function getDateOfCreation(): ?\DateTimeInterface
+    {
+        return $this->dateOfCreation;
+    }
+
+    public function setDateOfCreation(\DateTimeInterface $dateOfCreation): self
+    {
+        $this->dateOfCreation = $dateOfCreation;
+
+        return $this;
+    }
+
+    public function getDateLastMod(): ?\DateTimeInterface
+    {
+        return $this->dateLastMod;
+    }
+
+    public function setDateLastMod(\DateTimeInterface $dateLastMod): self
+    {
+        $this->dateLastMod = $dateLastMod;
+
+        return $this;
+    }
+}
