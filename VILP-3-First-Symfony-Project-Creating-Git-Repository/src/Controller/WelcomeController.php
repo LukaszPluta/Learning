c619419bdad2ad80543ff69df13844dc788b5447
lpluta <l.pluta@wearevirtua.com>
2019-01-22 14:57:34 +0100

    VILP-18

--- VILP-3-First-Symfony-Project-Creating-Git-Repository/src/Controller/WelcomeController.php
+++ VILP-3-First-Symfony-Project-Creating-Git-Repository/src/Controller/WelcomeController.php
@@ -4,16 +4,27 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Entity\ProductCategory;
+use App\Form\ProductCategoryType;
+use App\Repository\ProductCategoryRepository;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController  extends AbstractController
 {
-    public function hello()
+
+    public function hello(ProductCategoryRepository $productCategoryRepository)
     {
-        $date = date('m/d/Y h:i:s a');
+/*        $date = date('m/d/Y h:i:s a');
 
         return $this->render('firstTwig.html.twig',[
             'date'=>$date,
-        ]);
-
+        ]);*/
+        return $this->render('mainpage.html.twig',['products' => $productCategoryRepository->findAll(),]);
     }
+
+
 }
